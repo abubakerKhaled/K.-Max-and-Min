ce884e1df0d05d1f57844943f91525a35683efbc
abubaker khaled <abubakr_20210011@fci.helwan.edu.eg>
2023-05-14 21:06:04 +0300

    Problem Solving

--- main.cpp
+++ main.cpp
@@ -8,15 +8,16 @@ int main() {
     long num1, num2, num3;
     cin>>num1>>num2>>num3;
 
-    cout << min(num1, num2, num3) << " " << max(num1, num2, num3);
+    cout << min(num1, num2, num3) << " " << max(num1, num2, num3) << endl;
+
     return 0;
 }
 
 long max (long n1, long n2, long n3)
 {
-    if (n1 > n2 && n1 > n3) {
+    if ((n1 > n2 && n1 > n3) || (n1 == n2 && n1 > n3) || (n1 == n3 && n1 > n2)) {
         return n1;
-    } else if (n2 > n1 && n2 > n3) {
+    } else if ((n2 > n1 && n2 > n3) || (n2 == n3 && n2 > n1)) {
         return n2;
     } else {
         return n3;
@@ -25,9 +26,9 @@ long max (long n1, long n2, long n3)
 
 long min (long n1, long n2, long n3)
 {
-    if (n1 < n2 && n1 < n3) {
+    if ((n1 < n2 && n1 < n3) || (n1 == n2 && n1 < n3) || (n1 == n3 && n1 < n2)) {
         return n1;
-    } else if (n2 < n1 && n2 < n3) {
+    } else if ((n2 < n1 && n2 < n3) || (n2 == n3 && n2 < n1)) {
         return n2;
     } else {
         return n3;
